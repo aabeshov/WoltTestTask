baea50af663354e31d2e321eb6d4aad5041bcb8e
Ubuntu <ubuntu@ip-172-31-24-26.eu-central-1.compute.internal>
2024-07-18 07:14:01 +0000

    fix the code

--- task/views/cbv_views.py
+++ task/views/cbv_views.py
@@ -19,37 +19,38 @@ class calc_delivery_fee(APIView):
             total_fee = 0
             Delivery = Order_detail(**serializer.data)
 
-            if Delivery.cart_value > 10000:
-                return Response({"delivery_fee": 0}, status=status.HTTP_200_OK)
-            # Cart Value Condition
-            if Delivery.cart_value < 1000:
-                total_fee += 10 - (Delivery.cart_value / 100)
-                print(total_fee)
-            # Delivery Distance Conditions
-            if Delivery.delivery_distance <= 500:
-                total_fee += 1
-            else:
-                total_fee += (ceil(Delivery.delivery_distance / 500)) * 100
-            # Total items Conditions
-            if Delivery.number_of_items >= 5:
-                total_fee += (Delivery.number_of_items - 4) * 50
-                if Delivery.number_of_items > 12:
-                    total_fee += (Delivery.number_of_items - 12) * 1.2
+        if Delivery.cart_value > 10000:
+            return Response({"delivery_fee": 0}, status=status.HTTP_200_OK)
+        total_fee = 0
+        # Cart Value Condition
+        if Delivery.cart_value < 1000:
+            total_fee += 10 - (Delivery.cart_value / 100)
+        
+        # Delivery Distance Conditions
+        if Delivery.delivery_distance <= 500:
+            total_fee += 1
+        else:
+            total_fee += (ceil(Delivery.delivery_distance / 500)) * 100
 
-            # Rush Hour
-            time_now = datetime.fromisoformat(Delivery.time[:-1])
-            if datetime.weekday(time_now) == 5:
-                if 13 <= time_now.hour <= 19:
-                    total_fee *= 1.2
+        # Total items Conditions
+        if Delivery.number_of_items >= 5:
+            total_fee += (Delivery.number_of_items - 4) * 50
+            if Delivery.number_of_items > 12:
+                total_fee += (Delivery.number_of_items - 12) * 1.2
 
-            # Final Condition
-            if total_fee > 1500:
-                total_fee = 1500
+        # Rush Hour
+        time_now = datetime.fromisoformat(Delivery.time[:-1])
+        if datetime.weekday(time_now) == 5:
+            if 13 <= time_now.hour <= 19:
+                total_fee *= 1.2
 
-            return Response({"delivery_fee": int(total_fee)}, status=status.HTTP_200_OK)
+        # Final Condition
+        if total_fee > 1500:
+            total_fee = 1500
 
-        # Error return
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"delivery_fee": int(total_fee)}, status=status.HTTP_200_OK)
+    # Error return
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # Using POST method, can't be cached, more safe
     def post(self, request):
